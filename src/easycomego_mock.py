befd0dac7de6968dbac5a026f02971ff62733285
irvan <irvan.ramadhan@expecc.com>
2024-05-19 18:49:09 +0700

    Initial commit

---
+++ src/easycomego_mock.py
@@ -0,0 +1,66 @@
+import sys
+import json
+from flask import Flask, request, jsonify, make_response, Response
+import pprint
+import logging
+from logging.handlers import TimedRotatingFileHandler, RotatingFileHandler
+import uuid
+import socket
+from datetime import datetime, timedelta
+import os
+
+app = Flask(__name__)
+
+# CONSTANTS
+api_host = socket.gethostname()
+api_port = 39000
+api_id = "easycomego_mock_ws"
+
+# Work directory setup
+script_dir = os.path.dirname(os.path.realpath(__file__))
+home_dir = "/".join(script_dir.split("/")[:-1])
+log_dir = "{home_dir}/logs".format(home_dir=home_dir)
+
+DEP_CODE_LIST = [
+   "EASY-MY-PRT-KLANG",
+   "EASY-MY-BU",
+   "EASY-SG-HF",
+   "EASY-SG-BV"
+]
+
+DEST_CODE_LIST = [
+   "EASY-MY-PRT-KLANG",
+   "EASY-MY-BU",
+   "EASY-SG-HF",
+   "EASY-SG-BV"
+]
+
+TRANS_TYPE_LIST = [
+   "BUS",
+   "SHIP",
+   "VAN",
+   "MPV",
+   "EXEC_TAXI"
+]
+
+ROUTES_LIST = []
+for i in DEP_CODE_LIST:
+   for j in DEST_CODE_LIST:
+      for k in TRANS_TYPE_LIST:
+         ROUTES_LIST.append({
+            "transportCode": k,
+            "departureCode": i,
+            "destinationCode": j
+         })
+      # end for
+   # end for
+# end for
+
+@app.route('/getRoutes', methods=['POST'])
+def getRoutes():
+  return jsonify(ROUTES_LIST)
+# end def
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=api_port)
+# end if
